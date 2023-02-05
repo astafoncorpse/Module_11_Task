73db1f3bdce6f2ecbbfb4d5ac2fde763dba715e1
SkillFactoryCSharpTest <astafoncorpse@gmail.com>
2023-02-05 18:49:10 +0300

    Добавьте файлы проекта.

---
+++ Module_11_Task/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,45 @@
+﻿using Module_11_Task.Services;
+using System.Threading.Tasks;
+using System.Threading;
+using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Module_11_Task.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly IStorage _memoryStorage;
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            if (callbackQuery?.Data == null)
+                return;
+
+            string text = string.Empty;
+
+            _memoryStorage.GetSession(callbackQuery.From.Id).ActionCode = callbackQuery?.Data;
+
+            
+            if (callbackQuery?.Data == "LM")
+            {
+                text = $"Выбрано - Длина Сообщения.{Environment.NewLine}{Environment.NewLine}Введите сообщение";
+            }
+
+            if (callbackQuery?.Data == "Sum")
+            {
+                text = $"Выбрано - Сумма чисел.{Environment.NewLine}{Environment.NewLine}Введите числа через пробел";
+            }
+
+            await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, text, cancellationToken: ct);
+        }
+    }
+}
---
+++ Module_11_Task/Controllers/TextMessageController.cs
@@ -0,0 +1,79 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Types.Enums;
+using Module_11_Task.Services;
+
+namespace Module_11_Task.Controllers
+{
+    public class TextMessageController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+        private readonly IStorage _memoryStorage;
+
+        public TextMessageController(ITelegramBotClient telegramBotClient, IStorage memoryStorage)
+        {
+            _telegramClient = telegramBotClient;
+            _memoryStorage = memoryStorage;
+        }
+
+        public async Task Handle(Message message, CancellationToken ct)
+        {
+            
+            if (message.Text == "/start")
+            {
+                Console.WriteLine("Отправлено меню выбора");
+
+                var buttons = new List<InlineKeyboardButton[]>();
+                buttons.Add(new[]
+                {
+                        InlineKeyboardButton.WithCallbackData($" Длинна сообщения" , $"LM"),
+                        InlineKeyboardButton.WithCallbackData($" Сумма чисел" , $"Sum")
+                });
+
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, $"  Мой бот умеет считать длинну сообщения или сумму цифр. {Environment.NewLine}" +
+                    $"{Environment.NewLine}Выбери действие.{Environment.NewLine}", cancellationToken: ct, replyMarkup: new InlineKeyboardMarkup(buttons));
+            }
+            else 
+            {
+                string text = string.Empty;
+                var actionCode = _memoryStorage.GetSession(message.From.Id).ActionCode;
+
+                
+                if (actionCode == "LM")
+                {
+                    text = $"Длина сообщения равна {message.Text.Length}";
+                }
+
+                
+                if (actionCode == "Sum")
+                {
+                    
+                    var numbersArray = message.Text.Split(' '); 
+                    var sumResult = 0;
+                   
+                    foreach (string number in numbersArray)
+                    {
+                        
+                        if (int.TryParse(number, out int parseResult))
+                        {
+                            sumResult += parseResult;
+                        }
+                    }
+
+                    text = $"сумма чисел равна {sumResult}";
+                }
+
+
+                _memoryStorage.GetSession(message.From.Id).ActionCode = string.Empty;
+
+                text += $"{Environment.NewLine}. Напишите команду /start снова чтобы перейти в главное меню";
+                
+              
+                await _telegramClient.SendTextMessageAsync(message.Chat.Id, text, cancellationToken: ct);
+            }
+
+           
+        }
+    }
+}
